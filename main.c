beedd2efa6a735a7ea37115bcdc4dadc8cb4d8b4
Racamposx <rafaelnepomuceno99@gmail.com>
2021-10-03 01:07:43 -0300

    setting functionality calls

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
+    if(argc > 3){
+        // to be implemented - view functionality call
+        if(strcmp(argv[1], "--dot") == 0){
+            return 0;
+        }
 
-    
-    return 0;
+        // to be implemented - complement functionality call
+        if(strcmp(argv[1], "--complemento") == 0){
+            return 0;
+        }
+        
+        // to be implemented - intersection functionality call
+        if(strcmp(argv[1], "--intersecao") == 0){
+            return 0;
+        }
+
+        // to be implemented - union functionality call
+        if(strcmp(argv[1], "--uniao") == 0){
+            return 0;
+        }
+        
+        // to be implemented - minimization functionality call
+        if(strcmp(argv[1], "--minimizacao") == 0){
+            return 0;
+        }
+
+        // to be implemented - minimization functionality call
+        if(strcmp(argv[1], "--reconhecer") == 0){
+            return 0;
+        }
+
+        printf(">>>Error: Could not recognize command: %s", argv[1]);
+        printf("\n Use the correct sentence!!!");
+        return -1;
+    }
+
+    printf(">>>>Error: Invalid arguments number");
+    return -1;
 }
